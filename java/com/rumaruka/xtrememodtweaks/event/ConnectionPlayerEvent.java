2b0455b2d15b1ec0bc039699cd0f6c493866a033
Rumaruka <rumarukakunnya@gmail.com>
2015-11-19 13:21:55 +0500

    Message in chat. Attention =)

--- java/com/rumaruka/xtrememodtweaks/event/ConnectionPlayerEvent.java
+++ java/com/rumaruka/xtrememodtweaks/event/ConnectionPlayerEvent.java
@@ -1,7 +1,6 @@
 package com.rumaruka.xtrememodtweaks.event;
 
 import com.google.common.eventbus.Subscribe;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 import net.minecraft.util.ChatComponentText;
@@ -13,7 +12,7 @@ public class ConnectionPlayerEvent {
 	@SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
 
-		event.player.addChatComponentMessage(new ChatComponentText("For a comfortable game, it is recommended do not put value 'false' mobGriefing. Otherwise, you will not be able to play!"));
+	event.player.addChatComponentMessage(new ChatComponentText("For a comfortable game, it is recommended do not put value 'false' mobGriefing. Otherwise, you will not be able to play!"));
 		
     }
 
